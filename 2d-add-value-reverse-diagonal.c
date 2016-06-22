7f318854986308853c3b7367862756d142bfa0e5
Md. Imdadul Hoque <imdadul.ewubd@gmail.com>
2016-06-22 11:32:35 +0600

    Create 2d-add-value-reverse-diagonal.c

---
+++ 2d-add-value-reverse-diagonal.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+    int x,y,sum=0;
+    int a[4][4];
+    printf("Inter value in 4x4 array\n");
+    for(x=0;x<4;x++)
+    {
+        for(y=0;y<4;y++)
+        {
+            scanf("%d",&a[x][y]);
+        }
+    }
+    printf("Value taken finished!\nShowing value.\n");
+    for(x=0;x<4;x++)
+    {
+        for(y=0;y<4;y++)
+        {
+            if(x==(3-y))
+            {
+                sum=sum+a[x][y];
+            }
+        }
+    }
+    printf("%d\n",sum);
+}
